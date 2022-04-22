d5c0483a95145213fe2e1700ea71138536ec51aa
ElangGilangBuana <buana.183040043@mail.unpas.ac.id>
2022-04-22 20:48:13 +0700

    First Commit

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar fixed-top  shadow-sm" style="background-color: rgb(124, 223, 236)">
+      <div class="container">
+        <a class="navbar-brand">Medical Record Sapi</a>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Ear Tag" aria-label="Search" />
+          <button class="btn btn-outline-dark" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
---
+++ app/Views/sapi/index.php
@@ -0,0 +1,73 @@
+  <?= $this->extend('layout/template'); ?>
+
+  <?= $this->section('content'); ?>
+
+    <!-- Jumbotron -->
+    <section class="jumbotron" id="jumbotron"> 
+      <div class="container ">
+      <div class="text-center">
+          <h1 class="display-4">Medical Record Sapi</h1>
+          <hr class="my-4" /> 
+            <h2>Daftar Sapi</h2><br>
+            <div class="row">
+          <div class="col-5">
+            <!-- <form action="" method="post"> -->
+            <!-- <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Eartag Sapi  " name="keyword">
+  <div class="input-group-append">
+    <button class="btn btn-primary" type="submit" name="submit">Cari</button> -->
+  </div>
+</div>
+<!-- </form> -->
+          <!-- </div>
+        </div> -->
+        
+        
+        
+          
+          <table class="table table-sm table-bordered" >
+
+     <!-- Table -->
+    </thead>
+      <thead>
+        <tr>
+          <th scope="col">No.</th>
+          <th scope="col">Ear Tag</th>
+          <th scope="col">Nomor Hospital Pen</th>
+          <th scope="col">Penyakit</th>
+          <th scope="col">Detail</th>
+        </tr>
+
+        <tbody>
+       <?php $i = 1; ?> 
+      <?php foreach($sapi as $s) : ?>
+    <tr>
+      <th scope="row"><?= $i++; ?></th>
+      <td><?= $s->eartag ?></td>
+      <td><?= $s->id_hospital_pen ?></td>
+      <td><?= $s->nama_penyakit ?></td>
+      <td>
+          <a href="/detail/<?= $s->eartag ?>" class="btn btn-primary">Detail Sapi</a>
+          <a href="/kesehatan/<?= $s->eartag ?>" class="btn btn-success">Kesehatan</a>
+      </td>
+    </tr>
+    <?php endforeach;  ?>
+  </tbody>
+</table>
+ 
+        </div>
+    </div>
+</div>
+</section>
+<?= $this->endSection(); ?>
+
+ 
+
+  
+
+    
+   
+
+
+  
+  
